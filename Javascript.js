923433a97b1b1020b0fba11aaac06fc17805c26e
MeganLaw <meganlaw1028@gmail.com>
2022-11-29 07:32:01 +0000

    renew

--- Javascript.js
+++ Javascript.js
@@ -26,72 +26,73 @@
 //
 
 //function of the mailing list
-            
+             
     function addClient()
-        { 
-            var lastname=document.sample.lastname.value; 
-            var email=document.sample.email.value; 
-            
-            var tr = document.createElement('tr');
-            
-            var td1 = tr.appendChild(document.createElement('td'));
-            var td2 = tr.appendChild(document.createElement('td'));
-            var td3 = tr.appendChild(document.createElement('td'));
-            var td4 = tr.appendChild(document.createElement('td'));
-            
-            
-            td1.innerHTML=lastname;
-            td2.innerHTML=email;
-            td3.innerHTML='<input type="button" name="del" value="Delete" onclick="delClient(this);" class="btn-danger">'
-            td4.innerHTML='<input type="button" name="up" value="Update" onclick="UpClient(this);" class="btn-primary">'
+    { 
+        var lastname=document.sample.lastname.value; 
+        var email=document.sample.email.value; 
+        
+        var tr = document.createElement('tr');
+        
+        var td1 = tr.appendChild(document.createElement('td'));
+        var td2 = tr.appendChild(document.createElement('td'));
+        var td3 = tr.appendChild(document.createElement('td'));
+        var td4 = tr.appendChild(document.createElement('td'));
+        
+        
+        td1.innerHTML=lastname;
+        td2.innerHTML=email;
+        td3.innerHTML='<input type="button" name="del" value="Delete" onclick="delClient(this);" class="btn-danger">'
+        td4.innerHTML='<input type="button" name="up" value="Update" onclick="UpClient(this);" class="btn-primary">'
 
-            document.getElementById("tbl").appendChild(tr);
-        }
+        document.getElementById("tbl").appendChild(tr);
+    }
 
-        function UpClient(Client){
-            var lastname=document.sample.lastname.value; 
-            var email=document.sample.email.value; 
-            var s = Client.parentNode.parentNode;
-            var tr = document.createElement('tr');
-            
-            var td1 = tr.appendChild(document.createElement('td'));
-            var td2 = tr.appendChild(document.createElement('td'));
-            var td3 = tr.appendChild(document.createElement('td'));
-            var td4 = tr.appendChild(document.createElement('td'));
-            
-            
-            td1.innerHTML='<input type="text" name="lastname1">';
-            td2.innerHTML='<input type="text" name="email1">';
-            td3.innerHTML='<input type="button" name="del" value="Delete" onclick="delClient(this);" class="btn-danger">'
-            td4.innerHTML='<input type="button" name="up" value="Update" onclick="addUpClient(this);" class="btn-primary">'
+    function UpClient(Client){
+        var lastname=document.sample.lastname.value; 
+        var email=document.sample.email.value; 
+        var s = Client.parentNode.parentNode;
+        var tr = document.createElement('tr');
+        
+        var td1 = tr.appendChild(document.createElement('td'));
+        var td2 = tr.appendChild(document.createElement('td'));
+        var td3 = tr.appendChild(document.createElement('td'));
+        var td4 = tr.appendChild(document.createElement('td'));
+        
+        
+        td1.innerHTML='<input type="text" name="lastname1">';
+        td2.innerHTML='<input type="text" name="email1">';
+        td3.innerHTML='<input type="button" name="del" value="Delete" onclick="delClient(this);" class="btn-danger">'
+        td4.innerHTML='<input type="button" name="up" value="Update" onclick="addUpClient(this);" class="btn-primary">'
 
-            document.getElementById("tbl").replaceChild(tr, s);
-        }
+        document.getElementById("tbl").replaceChild(tr, s);
+    }
 
-        function addUpClient(Client){
-            var lastname=document.sample.lastname1.value; 
-            var email=document.sample.email1.value; 
-            var s = Client.parentNode.parentNode;
-            var tr = document.createElement('tr');
-            
-            var td1 = tr.appendChild(document.createElement('td'));
-            var td2 = tr.appendChild(document.createElement('td'));
-            var td3 = tr.appendChild(document.createElement('td'));
-            var td4 = tr.appendChild(document.createElement('td'));
-            
-            
-            td1.innerHTML=lastname;
-            td2.innerHTML=email;
-            td3.innerHTML='<input type="button" name="del" value="Delete" onclick="delClient(this);" class="btn-danger">'
-            td4.innerHTML='<input type="button" name="up" value="Update" onclick="UpClient(this);" class="btn-primary">'
+    function addUpClient(Client){
+        var lastname=document.sample.lastname1.value; 
+        var email=document.sample.email1.value; 
+        var s = Client.parentNode.parentNode;
+        var tr = document.createElement('tr');
+        
+        var td1 = tr.appendChild(document.createElement('td'));
+        var td2 = tr.appendChild(document.createElement('td'));
+        var td3 = tr.appendChild(document.createElement('td'));
+        var td4 = tr.appendChild(document.createElement('td'));
+        
+        
+        td1.innerHTML=lastname;
+        td2.innerHTML=email;
+        td3.innerHTML='<input type="button" name="del" value="Delete" onclick="delClient(this);" class="btn-danger">'
+        td4.innerHTML='<input type="button" name="up" value="Update" onclick="UpClient(this);" class="btn-primary">'
 
-            document.getElementById("tbl").replaceChild(tr, s);
-        }
+        document.getElementById("tbl").replaceChild(tr, s);
+    }
 
-        function delClient(Client){
-            var s=Client.parentNode.parentNode;
-            s.parentNode.removeChild(s);
-        }
+    function delClient(Client){
+        var s=Client.parentNode.parentNode;
+        s.parentNode.removeChild(s);
+    }
+
 
 //End of mailing List 
 
@@ -101,7 +102,7 @@
             var input, filter, table, tr, td, i, txtValue;
             input = document.getElementById("myInput");
             filter = input.value.toUpperCase();
-            table = document.getElementById("myTable");
+            table = document.getElementById("Client_Record");
             tr = table.getElementsByTagName("tr");
         
             // Loop through all table rows, and hide those who don't match the search query
